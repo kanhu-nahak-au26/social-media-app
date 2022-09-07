2f533bb2b020295342ca45f607723bb356e94130
Kanhu <kanhu.kcn@gmail.com>
2022-09-07 18:55:35 +0530

    final

---
+++ client/src/Data/TrendData.js
@@ -0,0 +1,24 @@
+export const TrendData= [
+  {
+    name: "Elon Musk",
+    shares: 71.9,
+  },
+  {
+    name: "Minions",
+    shares: 97,
+  },
+  {
+    name: "Avangers",
+    shares: 80.5,
+  },
+  
+  {
+    name: "Reactjs",
+    shares: 72,
+  },
+  
+  {
+    name: "Need for Speed",
+    shares: 20,
+  },
+];
